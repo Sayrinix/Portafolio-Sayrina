93fcaae3f723662606d84fb2578be463f26bbbbc
Sayrinix <sa.luciano@duocuc.cl>
2026-06-30 00:57:36 -0400

    paginas

---
+++ p_contacto.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Contacto
+*/
+get_header();
+?>
+
+<?php get_footer(); ?>
---
+++ proyectos.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Proyectos
+*/
+get_header();
+?>
+
+<?php get_footer(); ?>
