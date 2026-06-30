82e2ed45e34361389aa3941483c24327b93a0cfa
Sayrinix <sa.luciano@duocuc.cl>
2026-06-30 02:25:20 -0400

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -5,9 +5,9 @@ Template Name: Home
 get_header();
 ?>
 <main>
-<div class="container-fluid bg-image">
-<div class="w-100 d-flex justify-content-start ms-5 ps-4 align-content-around flex-wrap h-100">
-    <div class="mb-5">
+<div class="container-fluid bg-image pb-5">
+<div class="w-100 d-flex justify-content-start ms-5 ps-4 align-items-end h-100">
+    <div>
         <p class="p-light">Sayrina Luciano - UX Designer & Web Developer</p>
         <h1 class="h1-light">Cada proyecto cuenta una<br>historia. Esta es la mía.</h1>
     </div>
