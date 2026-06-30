f903df1f773628fc426db095ca08bb93c0159471
Sayrinix <sa.luciano@duocuc.cl>
2026-06-30 02:42:47 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,42 @@
-<footer class="site-footer">
-    <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+<footer class="bg-color-footer mt-5">
+    <div class="row d-flex pt-3 pb-3">
+        <div class="col-2">
+            <ul>
+                <li class="menu-texto pb-3">Home</li>
+                <li class="menu-texto">¿Quién soy?</li>
+            </ul>
+        </div>
+        <div class="col-2">
+            <ul>
+                <li class="menu-texto pb-3">Contacto</li>
+                <li class="menu-texto">Instagram</li>
+            </ul>
+        </div>
+        <div class="col-2">
+            <ul>
+                <li class="menu-texto pb-3">Correo</li>
+                <li class="menu-texto">Whatsapp</li>
+            </ul>
+        </div>
+        <div class="col-2">
+            <ul>
+                <li class="menu-texto pb-3">Github</li>
+                <li class="menu-texto">Descargar CV</li>
+            </ul>
+        </div>
+        <div class="col-2">
+            <ul>
+                <li class="menu-texto pb-3">Proyectos</li>
+                <li class="menu-texto">Blog</li>
+            </ul>
+        </div>
+        <div class="col-2">
+            <ul>
+                <li class="menu-texto">Diseñadora y Desarrolladora:<br>Sayrina Luciano</li>
+            </ul>
+        </div>
     </div>
-</footer>
+</footer> 
 
 <?php wp_footer(); ?>
 </body>
