f7bed6846dd22dcf9e4806631706d924eb091603
Sayrinix <sa.luciano@duocuc.cl>
2026-07-01 04:36:23 -0400

    ajustes

--- front-page.php
+++ front-page.php
@@ -18,7 +18,7 @@ get_header();
         <h2 class="h2-brown pb-5 text-center">Próximos Proyectos</h2>
         <div class="d-flex mt-5 altura-div2">
             <img class="me-2 imagen-arreglo" src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/proyecto-destacado.png')?>" alt="Persona Mayor Mujer usando un celular">
-            <div class="col-4 fondo-destacado d-flex flex-column justify-content-center align-items-center">
+            <div class="col-lg-4 fondo-destacado d-flex flex-column justify-content-center align-items-center">
                 <p class="m-3 pb-3 text-center p-brown">Este proyecto trata sobre una radio accesible para personas mayores.</p>
                 <a href="#" class="boton-ver">Ver más</a>
             </div>
@@ -29,7 +29,7 @@ get_header();
         <h2 class="h2-brown pb-5 text-center">¿Quién soy?</h2>
         <div class="d-flex mt-5 altura-div2">
             <img class="me-4 imagen-arreglo" src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/quien-soy-home.png')?>" alt="Paisaje de Girasoles pintados en óleo">
-            <div class="col-5 d-flex flex-column">
+            <div class="col-lg-5 d-flex flex-column">
                 <p class="p-brown pb-3">"El fin del arte y de la ciencia es servir a la vida"</p>
                 <div class="row">
                     <div class="col">
