b26b2795587ac2f27b2d302b18858b6c73568ccc
Sayrinix <sa.luciano@duocuc.cl>
2026-07-07 00:10:09 -0400

    a

--- p_quiensoy.php
+++ p_quiensoy.php
@@ -6,7 +6,7 @@ get_header();
 ?>
 <main>
 <div class="container-fluid bg-color-qn d-flex flex-column align-items-center">
-    <h1>Sayrina Luciano Jofré</h1>
-    <p>Diseñadora y Desarrolladora Web</p>
+    <h1 class="h1-brown">Sayrina Luciano Jofré</h1>
+    <p class="p-brown">Diseñadora y Desarrolladora Web</p>
 </div>
 <?php get_footer(); ?>
