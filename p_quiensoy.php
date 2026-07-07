2534935a3fe545f43eb8a89da3ae6e9647248634
Sayrinix <sa.luciano@duocuc.cl>
2026-07-07 00:34:43 -0400

    Update p_quiensoy.php

--- p_quiensoy.php
+++ p_quiensoy.php
@@ -11,8 +11,8 @@ get_header();
 </div>
 
 <div class="container mt-5 pt-5">
-    <div class="row">
-        <div class="col-6 pe-0 me-0">
+    <div class="row d-flex justify-content-center">
+        <div class="col-6 pe-3">
             <img class="arreglo-imagen-perfil" src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/foto-perfil.jpeg')?>" alt="">
         </div>
         <div class="col-4 bg-color-desc">
