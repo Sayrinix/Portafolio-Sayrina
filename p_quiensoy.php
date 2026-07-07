b8eacf656b84c0761668ce4c646eeff5b5d28956
Sayrinix <sa.luciano@duocuc.cl>
2026-07-07 01:21:37 -0400

    Update p_quiensoy.php

--- p_quiensoy.php
+++ p_quiensoy.php
@@ -23,15 +23,15 @@ get_header();
 <div class="container mt-5 pt-5">
     <div class="row text-center">
         <h2 class="h2-brown">Enfoque Profesional</h2>
-        <div class="col-4 enfoque-card">
+        <div class="col-3 enfoque-card">
             <h3 class="h3-brown pb-3">Soluciones digitales para personas mayores</h3>
             <p class="p-brown">Me interesa desarrollar soluciones digitales accesibles, simples y funcionales que faciliten la experiencia de uso de las personas mayores en entornos digitales.</p>
         </div>
-        <div class="col-4 enfoque-card">
+        <div class="col-3 enfoque-card">
             <h3 class="h3-brown pb-3">Posicionamiento Digital para emprendedores</h3>
             <p class="p-brown">Me interesa apoyar a emprendedores y pequeñas marcas en la construcción de una presencia digital sólida, estratégica y coherente. Este enfoque considera el desarrollo de sitios web, optimización SEO, estructura de contenidos e identidad digital/p>
         </div>
-        <div class="col-4 enfoque-card">
+        <div class="col-3 enfoque-card">
             <h3 class="h3-brown pb-3">Investigación en Seguridad Digital</h3>
             <p class="p-brown">Mi interés está especialmente orientado a acercar la seguridad digital a personas y grupos que no siempre cuentan con conocimientos técnicos, promoviendo una cultura digital más informada, responsable y preventiva.</p>
         </div>
