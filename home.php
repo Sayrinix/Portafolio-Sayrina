02b1efd8f02e0f2721eb3f033e5b9e9d6db7d7b2
Sayrinix <sa.luciano@duocuc.cl>
2026-07-07 22:47:40 -0400

    margen

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 <main>
-<div class="container altura-div1 d-flex align-items-bottom">
+<div class="container margen-cont-hero">
     <div class="row m-0 p-0">
         <div class="col-5 m-0 arreglo-hero-blog d-flex flex-column align-items-center justify-content-center text-center">
             <h1 class="h1-brown">Toda gran experiencia, comienza con una idea.</h1>
