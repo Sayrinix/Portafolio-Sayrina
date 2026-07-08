68485517b4dae6ee1d78f528967f18ba5dd7d9a0
Sayrinix <sa.luciano@duocuc.cl>
2026-07-07 22:54:35 -0400

    Update home.php

--- home.php
+++ home.php
@@ -3,7 +3,7 @@ get_header();
 ?>
 <main>
 <div class="container margen-cont-hero">
-    <div class="row m-0 p-0">
+    <div class="row g-0">
         <div class="col-5 m-0 arreglo-hero-blog d-flex flex-column align-items-center justify-content-center text-center">
             <h1 class="h1-brown">Toda gran experiencia, comienza con una idea.</h1>
             <p class="p-brown">¡Descubre nuestro Blog!</p>
