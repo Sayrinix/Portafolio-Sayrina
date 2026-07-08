458057a297d289a44b5d1a5634bd3f78e6907ad1
Sayrinix <sa.luciano@duocuc.cl>
2026-07-07 22:29:15 -0400

    hero blog

---
+++ p_blog.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: Blog
+*/
+get_header();
+?>
+<main>
+<div class="container mt-5 pt-5">
+    <div class="row m-0 p-0">
+        <div class="col-5 arreglo-hero-blog d-flex flex-column align-items-center justify-content-center text-center">
+            <h1 class="h1-brown">Toda gran experiencia, comienza con una idea.</h1>
+            <p class="p-brown">¡Descubre nuestro Blog!</p>
+        </div>
+        <div class="col-7">
+            <img class="arreglo-img-blog-hero" src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/blog-hero.png')?>" alt="">
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
