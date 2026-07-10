beb7ab43bcd08bdeb63c0b19a797fdcab4fb8180
Sayrinix <sa.luciano@duocuc.cl>
2026-07-10 19:36:39 -0400

    categorias tabs

--- home.php
+++ home.php
@@ -40,7 +40,7 @@ get_header();
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/placeholder.jpg" alt="Imagen por defecto" class="img-fluid">
                     <?php endif; ?>
                 </a>
-                <div>
+            <div>
                 <span class="p-brown-2 posicion-span-left"><?php echo get_the_date(); ?></span>
                 <?php
                 $categories = get_the_category();
@@ -50,11 +50,12 @@ get_header();
                         <?php echo esc_html($categories[0]->name); ?>
                     </span>
                 <?php endif; ?>                        
-                </div>
-                
+            </div>       
         </div>
+
         <?php endwhile; ?>
-        <div class="col-6 p-3 display-flex flex-column justify-content-center align-items-center">
+
+        <div class="col-6 ps-3 display-flex flex-column justify-content-center align-items-center">
             <h3><a class="h3-brown pb-2" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             <p class="p-brown-2"><?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?></p>
             <div class="w-auto d-flex justify-content-end">
@@ -69,5 +70,74 @@ get_header();
                 <p>No hay entradas publicadas todavía.</p>
 
             <?php endif; ?>
+</div>
+
+<div class="container">
+  <ul class="nav nav-underline" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="accesibilidad-tab" data-bs-toggle="tab" data-bs-target="#accesibilidad-tab-pane" type="button" role="tab" aria-controls="accesibilidad-tab-pane" aria-selected="true">Accesibilidad</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Profile</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Contact</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="disabled-tab" data-bs-toggle="tab" data-bs-target="#disabled-tab-pane" type="button" role="tab" aria-controls="disabled-tab-pane" aria-selected="false" disabled>Disabled</button>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="accesibilidad-tab-pane" role="tabpanel" aria-labelledby="accesibilidad-tab" tabindex="0">
+
+ <?php
+    $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+
+    $args = array(
+        'post_type'      => 'post',
+        'posts_per_page' => 4,
+        'paged'          => $paged,
+        'category_name'  => 'accesibilidad',
+    );
+
+    $blog_query = new WP_Query($args);
+    ?>
+
+    <?php if ($blog_query->have_posts()) : ?>
+        <div class="row">
+            <?php while ($blog_query->have_posts()) : $blog_query->the_post(); ?>
+            <div class="col-3 posicion-imagen-category{">
+                <a href="<?php the_permalink(); ?>">
+                        <?php if (has_post_thumbnail()) : ?>
+                            <?php the_post_thumbnail('large', array('class' => 'img-arreglo-category')); ?>
+                        <?php else : ?>
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/placeholder.jpg" alt="Imagen por defecto" class="img-fluid">
+                        <?php endif; ?>
+                    </a>
+                <div>
+                    <span class="p-brown-2 posicion-span-left"><?php echo get_the_date(); ?></span>
+                </div>
+            </div>
+            <?php endwhile; ?>
+            <div class="col-2 ps-3 display-flex flex-column justify-content-center align-items-center">
+                <h3><a class="h3-brown pb-2" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <p class="p-brown-2"><?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?></p>
+                <div class="w-auto d-flex justify-content-start">
+                    <a class="boton-ver-s" href="<?php the_permalink(); ?>">Ver más</a>
+                </div>
+            </div>
+        </div>
+<?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+
+    <p>No hay entradas publicadas todavía.</p>
+
+<?php endif; ?>
+  </div>
+  <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
+  <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">...</div>
+  <div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">...</div>
+</div>
 </div>
 <?php get_footer(); ?>
