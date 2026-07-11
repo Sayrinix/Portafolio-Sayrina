c35c9f7e17d7846895b17abc5d59d92d94a2bb89
Sayrinix <sa.luciano@duocuc.cl>
2026-07-10 20:31:22 -0400

    Update home.php

--- home.php
+++ home.php
@@ -5,7 +5,7 @@ get_header();
 <div class="container margen-cont-hero">
     <div class="row g-0">
         <div class="col-5 m-0 arreglo-hero-blog d-flex flex-column align-items-center justify-content-center text-center">
-            <h1 class="h1-brown">Toda gran experiencia, comienza con una idea.</h1>
+            <h1 class="h1-brown">Toda gran experiencia, comienza <br> con una idea.</h1>
             <p class="p-brown">¡Descubre nuestro Blog!</p>
         </div>
         <div class="col-7 m-0">
@@ -53,8 +53,6 @@ get_header();
             </div>       
         </div>
 
-        <?php endwhile; ?>
-
         <div class="col-6 ps-3 display-flex flex-column justify-content-center align-items-center">
             <h3><a class="h3-brown pb-2" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             <p class="p-brown-2"><?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?></p>
@@ -62,6 +60,7 @@ get_header();
                 <a class="boton-ver-s" href="<?php the_permalink(); ?>">Ver más</a>
             </div>
         </div>
+        <?php endwhile; ?>
     </div>
     <?php wp_reset_postdata(); ?>
 
