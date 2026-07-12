f4a7d00e9c0dc77d25cdb397b81c8b6955276203
Sayrinix <sa.luciano@duocuc.cl>
2026-07-12 00:52:30 -0400

    a

--- p_habilidades.php
+++ p_habilidades.php
@@ -6,7 +6,72 @@ get_header();
 ?>
 <main>
  <div class="metodologia-banner">
-
- </div>   
+ </div>
+ <div class="container">
+    <div class="row">
+       <h1 class="h1-brown text-center">Habilidades Blandas</h1>  
+    </div>
+    <div class="row bg-color-hab">
+        <div class="col-4">
+            <div class="row">
+                <div class="col">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/habilidad-1.jpg')?>" alt="Un hombre y una mujer trabajando y analizando datos.">
+                </div>
+                <div class="col">
+                    <h3 class="h3-brown">Titulo</h3>
+                    <p class="p-brown-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/habilidad-2.jpg')?>" alt="Un hombre y una mujer trabajando y analizando datos.">
+                </div>
+                <div class="col">
+                    <h3 class="h3-brown">Titulo</h3>
+                    <p class="p-brown-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/habilidad-3.jpg')?>" alt="Un hombre y una mujer trabajando y analizando datos.">
+                </div>
+                <div class="col">
+                    <h3 class="h3-brown">Titulo</h3>
+                    <p class="p-brown-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="row">
+                <div class="col">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/habilidad-4.jpg')?>" alt="Un hombre y una mujer trabajando y analizando datos.">
+                </div>
+                <div class="col">
+                    <h3 class="h3-brown">Titulo</h3>
+                    <p class="p-brown-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/habilidad-5.jpg')?>" alt="Un hombre y una mujer trabajando y analizando datos.">
+                </div>
+                <div class="col">
+                    <h3 class="h3-brown">Titulo</h3>
+                    <p class="p-brown-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/habilidad-6.jpg')?>" alt="Un hombre y una mujer trabajando y analizando datos.">
+                </div>
+                <div class="col">
+                    <h3 class="h3-brown">Titulo</h3>
+                    <p class="p-brown-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </div>    
+    </div>
+ </div>
+   
 </main>
 <?php get_footer(); ?>
