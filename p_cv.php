05ca0b2ce9dfac3a50d9ddcc8da472119e9b9b9b
Sayrinix <sa.luciano@duocuc.cl>
2026-07-15 02:21:35 -0400

    Update p_cv.php

--- p_cv.php
+++ p_cv.php
@@ -11,12 +11,7 @@ get_header();
   <div class="container d-flex justify-content-center w-100">
     <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
-
-         <div class="row">
-            <div class="col-12">
                  <?php the_content(); ?>
-            </div>       
-        </div>
             <?php endwhile; ?>
         <?php endif; ?>   
   </div> 
