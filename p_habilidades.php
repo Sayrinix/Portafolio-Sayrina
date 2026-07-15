1d0bfcc226caedf5c969c46aebaf5e0fd80d3d1d
Sayrinix <sa.luciano@duocuc.cl>
2026-07-15 00:10:08 -0400

    Update p_habilidades.php

--- p_habilidades.php
+++ p_habilidades.php
@@ -79,38 +79,38 @@ get_header();
  </div>
  <div id="carouselExample" class="carousel slide">
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <div class="text-center bg-color-tecnicas-1">
+    <div class="carousel-item active d-flex">
+      <div class="text-center bg-color-tecnicas-1 me-1">
         <h3 class="h3-brown">Diseño UX/UI</h3>
         <p class="p-brown"></p>
       </div>
-      <div class="text-center bg-color-tecnicas-2">
+      <div class="text-center bg-color-tecnicas-2 me-1">
         <h3 class="h3-brown">Diseño UX/UI</h3>
         <p class="p-brown"></p>
       </div>
-      <div class="text-center bg-color-tecnicas-1">
+      <div class="text-center bg-color-tecnicas-1 me-1">
         <h3 class="h3-brown">Diseño UX/UI</h3>
         <p class="p-brown"></p>
       </div>
-      <div class="text-center bg-color-tecnicas-2">
+      <div class="text-center bg-color-tecnicas-2 me-1">
         <h3 class="h3-brown">Diseño UX/UI</h3>
         <p class="p-brown"></p>
       </div>
     </div>
-    <div class="carousel-item">
-      <div class="text-center bg-color-tecnicas-1">
+    <div class="carousel-item d-flex">
+      <div class="text-center bg-color-tecnicas-1 me-1">
         <h3 class="h3-brown">Diseño UX/UI</h3>
         <p class="p-brown"></p>
       </div>
-      <div class="text-center bg-color-tecnicas-2">
+      <div class="text-center bg-color-tecnicas-2 me-1">
         <h3 class="h3-brown">Diseño UX/UI</h3>
         <p class="p-brown"></p>
       </div>
-      <div class="text-center bg-color-tecnicas-1">
+      <div class="text-center bg-color-tecnicas-1 me-1">
         <h3 class="h3-brown">Diseño UX/UI</h3>
         <p class="p-brown"></p>
       </div>
-      <div class="text-center bg-color-tecnicas-2">
+      <div class="text-center bg-color-tecnicas-2 me-1">
         <h3 class="h3-brown">Diseño UX/UI</h3>
         <p class="p-brown"></p>
       </div>
