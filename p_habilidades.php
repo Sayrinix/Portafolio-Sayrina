de360a0f368658a6c3a952d2ab5529f9efd75dea
Sayrinix <sa.luciano@duocuc.cl>
2026-07-15 04:13:14 -0400

    Update p_habilidades.php

--- p_habilidades.php
+++ p_habilidades.php
@@ -13,7 +13,7 @@ get_header();
  <div class="container-fluid mt-5 bg-color-hab d-flex flex-column justify-content-center align-items-center h-100">
     
         <div class="row d-flex justify-content-center">
-            <div class="col-6 me-5">
+            <div class="col-12 me-5">
                 <div class="row pb-5">
                     <div class="col-6">
                         <img class="arreglo-img-hab-bl" src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/habilidad-1.jpg')?>" alt="Un hombre y una mujer trabajando y analizando datos.">
@@ -42,7 +42,7 @@ get_header();
                     </div>
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-12">
                 <div class="row pb-5">
                     <div class="col-6">
                         <img class="arreglo-img-hab-bl" src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/habilidad-4.jpg')?>" alt="Un hombre y una mujer trabajando y analizando datos.">
