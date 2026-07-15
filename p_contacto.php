8e05d667b680f80249930dc719648308dc976b43
Sayrinix <sa.luciano@duocuc.cl>
2026-07-15 01:54:49 -0400

    Update p_contacto.php

--- p_contacto.php
+++ p_contacto.php
@@ -13,31 +13,31 @@ get_header();
         <div class="col-8">
 <form action="post" class="bg-color-contacto-form">
     <div class="row">
-        <div class="form-floating mb-3 col">
+        <div class="mb-3 col">
             <label for="floatingInput" class="form-label">Nombre</label>
         <input type="text" class="form-control form-control-sm" id="floatingInput" placeholder="Nombre">
         
         </div>
-        <div class="form-floating col">
+        <div class="mb-3 col">
             <label class="form-label" for="floatingEmpresa">Empresa</label>
             <input type="text" class="form-control form-control-sm" id="floatingEmpresa" placeholder="Empresa">
             
         </div>
     </div>
     <div class="row">
-       <div class="form-floating mb-3 col">
+       <div class="mb-3 col">
         <label class="form-label" for="floatingEmail">Correo Electrónico</label>
         <input type="email" class="form-control form-control-sm" id="floatingEmail" placeholder="nombre@ejemplo.com">
         
         </div>
-        <div class="form-floating mb-3 col">
+        <div class="mb-3 col">
             <label class="form-label" for="floatingTel">Número de Teléfono</label>
         <input type="tel" class="form-control form-control-sm" id="floatingTel" placeholder="+56 X XX XXXX XX">
         
         </div>
     </div>
     <div>
-        <div class="form-floating">
+        <div>
             <label class="form-label" for="floatingTextarea">Consulta</label>
         <textarea rows="3" class="form-control" placeholder="Escribe tu consulta..." id="floatingTextarea"></textarea>
         
